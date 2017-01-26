ab553c1db5efb39e2cfec3ec9ff2457c2f3b9db2
hwalborn <hwalborn@gmail.com>
2017-01-25 22:35:02 -0600

    finalized is_square

--- is_square.rb
+++ is_square.rb
@@ -1,14 +1,8 @@
-require 'pry'
-
 def is_square(x)
-  if x > 0
-    (1...x).each {|num|
-      if num * num == x
-        binding.pry
-        return true
-      end
-    }
+  if x >= 0
+    if (Math.sqrt(x) == Math.sqrt(x).floor)
+      return true
+    end
   end
   false
 end
-is_square(1444)
