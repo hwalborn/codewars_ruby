3cc8efb1b3bf9409e66d793442c40f4bdae03440
hwalborn <hwalborn@gmail.com>
2017-01-25 22:54:41 -0600

    added notes

--- high_and_low.rb
+++ high_and_low.rb
@@ -1,3 +1,6 @@
+#finds highest and lowest number in string of numbers and returns it
+#in a string of high number and low number
+
 def high_and_low(numbers)
   arr = numbers.split(" ").sort_by {|num| num.to_i}
   return "#{arr[-1]} #{arr[0]}"
--- is_square.rb
+++ is_square.rb
@@ -1,3 +1,6 @@
+#returns true if the given number is a perfect square
+#and false if it is not
+
 def is_square(x)
   if x >= 0
     if (Math.sqrt(x) == Math.sqrt(x).floor)
